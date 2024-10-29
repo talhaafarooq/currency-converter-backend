9614ab28e9fc860e6063dd9f69174f92a94d0e26
Talha Farooq <talhafarooq522446@gmail.com>
2024-10-29 13:25:46 +0500

    add currencies routes

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\API\{
 };
 
 // Authentication
-Route::middleware('throttle:60,1')->group(function(){
-    Route::post('login', [AuthController::class,'login']);
+Route::middleware('throttle:60,1')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
 
 
     // API routes for CurrencyController
@@ -16,7 +16,16 @@ Route::middleware('throttle:60,1')->group(function(){
         Route::get('/currencies', 'getCurrencies')->name('currencies.index');
         Route::post('/convert', 'convert')->name('currencies.convert');
     });
+
+
+    Route::prefix('currencies')
+        ->controller(CurrencyController::class)
+        ->middleware('auth:sanctum')
+        ->group(function () {
+            Route::get('/',  'index');
+            Route::post('/',  'store');
+            Route::put('/{id}',  'update');
+            Route::delete('/{id}',  'destroy');
+            Route::get('/search',  'search');
+        });
 });
-
-
-
